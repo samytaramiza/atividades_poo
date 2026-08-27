5726e6d77f0a9632329e38be281c34a44d2b3013
Samyta Ramiza <vicente.ramiza@escolar.ifrn.edu.br>
2026-08-27 20:10:28 -0300

    questão 1

---
+++ exercicio_slide01/questão1.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um valor inteiro para A:"))
+b = int(input("Digite um valor inteiro para B:"))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} / {b} = {a / b}")
